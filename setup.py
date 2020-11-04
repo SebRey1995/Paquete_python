c3a66b1cd69e616a340767d116319b297ac7bdf3
SebRey1995 <46502201+SebRey1995@users.noreply.github.com>
2020-11-04 17:35:53 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
-setup(name='Libreria1',
+setup(name='libreria1',
 version='0.1',
 description='Testing installation of Package',
 url='#',
 author='S.A. Reyes-Romero',
 author_email='rerosa@ier.unam.mx',
 license='NON',
-packages=['Libreria1'],
+packages=['libreria1'],
 zip_safe=False)
